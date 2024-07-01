f2b0dd09a163d988ce54e7ea7b2da1fa2c0c3704
gksmf2370 <gksmf2370@naver.com>
2024-07-01 10:16:11 +0900

    test

--- EX_PY06/DAY04/ex_dict_01.py
+++ EX_PY06/DAY04/ex_dict_01.py
@@ -16,3 +16,26 @@ print(f'data => {len(data)}개, {data}, {type(data)}')
 # 강아지에 대한 정보 : 품종, 무게, 색상, 성별, 나이
 data_dog={'kind':'치와와','weight':3, 'color':'갈색', 'gender':'남', 'age':3}
 print(f'data => {len(data_dog)}개, {data_dog}, {type(data_dog)}')
+
+##[dict 원소/요소 읽기] -------------------
+## - 키를 가지고 값/데이터 읽기
+## - 형식: 변수명[키]
+data_dog={'kind':'치와와','weight':3, 'color':'갈색', 'gender':'남', 'age':3}
+
+#색상 출력
+print(f'색상 : {data_dog["color"]}')
+
+#성별, 품종 출력
+print(f'성별 : {data_dog["gender"]}, 품종 : {data_dog["kind"]}')
+
+
+## [dict 원소/요소 변경] ---------------------
+## -변수명[키] = 새로운 값
+
+# 나이 5살 ==> 6살
+data_dog["age"] = 6
+print(data_dog)
+
+# 몸무게 3kg ===> 8kg
+data_dog["weight"] = 8
+print(data_dog)
