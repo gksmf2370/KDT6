80ca57108dfedbbb79a36c0da38b6ccd4cac5e8d
gksmf2370 <gksmf2370@naver.com>
2024-08-09 09:02:25 +0900

    baekjoon

---
+++ 백준노트/2941.py
@@ -0,0 +1,10 @@
+# č	c=          ć	c-        dž	dz=      đ	d-       lj	lj         nj	nj       š	s=        ž	z=
+# 간단하게 생각해보기 stack에서 꺼내온다고 생각하자
+a=input() 
+
+b=['c=', 'c-', ' dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+
+for i in b:
+    a=a.replace(i,'*')
+
+print(len(a))
